b373fac79d2ce5abf78693a73002f39a6e2aae5b
Director-888 <Director-888>
2026-06-14 23:30:13 +0500

    Add discount logic

--- main.go
+++ main.go
@@ -4,9 +4,21 @@ import "fmt"
 
 func main() {
 	var days int
-	fmt.Println("Сколько продлится поездка?")
+	fmt.Println("Сколько дней продлится поездка?")
 	fmt.Scanln(&days)
-	hotelprice := 4000
-	total := hotelprice * days
-	fmt.Println(total)
+
+	hotelPrice := 4000
+	total := hotelPrice * days
+
+	// Проверяем условие: если дней больше 7
+	if days > 7 {
+		fmt.Println("Ура! Вы получили скидку 10% за долгое бронирование!")
+		// Рассчитываем скидку (делим общую сумму на 10 и вычитаем её из total)
+		total = total - (total / 10)
+	} else {
+		fmt.Println("До скидки вам не хватило еще немного дней!")
+
+	}
+
+	fmt.Println("Итоговая стоимость отеля обойдется в", total, "рублей")
 }
