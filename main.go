a4a1e0580648d7483d37c81555362b87dd2a7d10
Director-888 <Director-888>
2026-06-14 23:45:58 +0500

    Add complex conditions

--- main.go
+++ main.go
@@ -7,16 +7,20 @@ func main() {
 	fmt.Println("Сколько дней продлится поездка?")
 	fmt.Scanln(&days)
 
+	var contry string
+	fmt.Println("В какую страну вы едите?")
+	fmt.Scanln(&contry)
+
 	hotelPrice := 4000
 	total := hotelPrice * days
 
 	// Проверяем условие: если дней больше 7
-	if days > 7 {
-		fmt.Println("Ура! Вы получили скидку 10% за долгое бронирование!")
+	if days > 10 && contry == "Грузия" {
+		fmt.Println("Ура! Вы получили скидку 20% как VIP клиент!")
 		// Рассчитываем скидку (делим общую сумму на 10 и вычитаем её из total)
-		total = total - (total / 10)
+		total = total - (total / 5)
 	} else {
-		fmt.Println("До скидки вам не хватило еще немного дней!")
+		fmt.Println("Скидка не применяется")
 
 	}
 
