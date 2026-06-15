f97e4bd13bea46a977f2cfc1b32203769a102911
Director-888 <Director-888>
2026-06-15 15:48:57 +0500

    Task 6

--- main.go
+++ main.go
@@ -3,24 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var totalPrice int
+	for i := 5; i >= 1; i-- {
 
-	ticketPrice := 10000
-
-	var age int
-	fmt.Println("Сколько вам лет?")
-	fmt.Scanln(&age)
-
-	if age < 7 {
-		fmt.Println("У вас скидка 50%!")
-		totalPrice = ticketPrice - (ticketPrice / 2)
-
-	} else if age > 65 {
-		fmt.Println("У вас скидка 30%")
-		totalPrice = ticketPrice - (ticketPrice * 30 / 100)
-	} else {
-		totalPrice = ticketPrice
+		fmt.Println("Поехали", i)
 	}
-	fmt.Println("Стоимость вашего билета", totalPrice, "рублей")
-
 }
