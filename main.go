da8afc7cfd00c6c5a6cf57dbacde426a817f8296
Director-888 <Director-888>
2026-06-15 11:51:43 +0500

    Solve task 3

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	fmt.Println("Как тебя зовут?")
-	fmt.Scanln(&name)
 
 	var age int
 	fmt.Println("Сколько тебе лет?")
 	fmt.Scanln(&age)
 
-	var city string
-	fmt.Println("В каком городе живешь?")
-	fmt.Scanln(&city)
+	if age >= 18 {
+		fmt.Println("Доступ разрешен!")
 
-	fmt.Println("Привет", name, "Тебе", age, "лет", "и ты живешь в городе", city)
+	} else {
+		fmt.Println("Доступ запрещен, вам нужно подрасти!")
+	}
 }
